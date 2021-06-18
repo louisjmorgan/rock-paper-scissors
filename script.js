33bad6ac6f14af203558e90a0c76d39587aed2b0
louisjmorgan <louis.morgan@hotmail.com>
2021-06-18 13:31:20 +0100

    Finished console-based rock paper scissors game

---
+++ script.js
@@ -0,0 +1,66 @@
+function computerPlay() {
+    let rand = Math.random();
+    ul = 2 / 3;
+    ll = 1 / 3;
+    if (rand <= ll) {
+        return "rock";
+    }
+    else if (rand > ll && rand <= ul) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function playRound (computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        return;
+    }
+    else if (computerSelection == "rock" && playerSelection == "scissors" || 
+    computerSelection == "paper" && playerSelection == "rock" || 
+    computerSelection == "scissors" && playerSelection == "paper") {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function game () {
+    let playerScore = parseInt(0);
+    let computerScore = parseInt(0);
+
+    while (playerScore < 3 && computerScore < 3) {
+        computerSelection = computerPlay();
+        playerSelection = prompt("Choose your fighter:").toLowerCase();
+
+        result = playRound(computerSelection, playerSelection);
+
+        if (result == true) {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
+        }
+
+        else if (result == false) {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+            computerScore++;
+        }
+
+        else {
+            console.log(`Tie! You both selected ${computerSelection}`)
+        }
+
+        console.log(`The score is player: ${playerScore}, computer: ${computerScore}`);
+    }
+
+    if (playerScore == 3) {
+        console.log(`Congratulations! You win ${playerScore}-${computerScore}.`);
+    }
+
+    else {
+        console.log(`The computer won ${computerScore}-${playerScore}. Better luck next time!`);
+    }
+}
+
+game()
